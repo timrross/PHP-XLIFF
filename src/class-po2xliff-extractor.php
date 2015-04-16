23e90df56211be80a100aab8061dcf10b64d4cc2
Hew <hewsut@gmail.com>
2015-04-15 22:08:11 -0400

    Add WP Post and PO file content extractors
    These need more work. It'd be need to make some more extractors for common content sources as well.

---
+++ src/class-po2xliff-extractor.php
@@ -0,0 +1,37 @@
+<?php
+class Po2Xliff_Extractor {
+	public static function extract( $po ) {
+		if ( ! $po instanceof PO ) {
+			return false;
+		}
+
+		$xliff = new Xliff_Document();
+		$xliff->set_source_locale( get_locale() );
+		$xliff->set_target_locale( '%%%%trgLang%%%%');
+		$xliff->file(true)->set_attribute( 'id', $po->headers['Project-Id-Version'] );
+
+		$create_new_notes = true;
+		$count = 0;
+
+		foreach ( $po->entries as $entry ) {
+			$xliff->file()->unit(true)->set_attribute( 'id', 'entry-' . $count );
+
+			if ( ! empty( $entry->context ) ) {
+				$xliff->file()->unit()->notes(true)->note(true)->set_text_content( $entry->context );
+				$create_new_notes = false;
+			}
+
+			if ( ! empty( $entry->extracted_comments ) ) {
+				$xliff->file()->unit()->notes( $create_new_notes )->note(true)->set_text_content( $entry->extracted_comments );
+			}
+
+			$xliff->file()->unit()->segment(true)->source(true)->set_text_content( $entry->singular );
+			$xliff->file()->unit()->segment()->target(true);
+
+			$create_new_notes = true;
+			$count++;
+		}
+
+		return $xliff;
+	}
+}
